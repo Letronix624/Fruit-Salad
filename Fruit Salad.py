03938d83c0af7c9d20ca69eb03776a60c9c13b8c
Let <94926994+Letronix624@users.noreply.github.com>
2022-02-13 18:46:31 +0100

    Settings furthered and languages added
    there is now a language option.

--- Fruit Salad.py
+++ Fruit Salad.py
@@ -15,6 +15,7 @@ def mainwindow():
     windowvisible = True
     traymenu.update_menu()
     root = tkinter.Tk()
+    root.configure(bg='#303136')
     startbuttonanimation = [
         ImageTk.PhotoImage(file=f"{pydir}\\GUI\\On Switch 1.png"),
         ImageTk.PhotoImage(file=f"{pydir}\\GUI\\On Switch 2.png"),
@@ -30,11 +31,11 @@ def mainwindow():
     root.wm_attributes("-transparentcolor", '#010110')
     #Stats
     tkinter.Label(root, text=f'{hashrate} mh/s', bg='#303136', fg="white", font=fontextremelybig).place(x=425, y=480, width=350, height=70)
-    tkinter.Label(root, text=f"Gpu: {gpuname['gpuname']}", bg='#303136', fg="white", font=fontbig, anchor=tkinter.W).place(x=0, y=30, width=400, height=50)
-    tkinter.Label(root, text='Miner:', bg='#303136', fg="white", font=fontbig, anchor=tkinter.W).place(x=0, y=80, width=400, height=50)
-    tkinter.Label(root, text='Algo:', bg='#303136', fg="white", font=fontbig, anchor=tkinter.W).place(x=0, y=130, width=400, height=50)
-    tkinter.Label(root, text='Pool:', bg='#303136', fg="white", font=fontbig, anchor=tkinter.W).place(x=0, y=180, width=400, height=50)
-    tkinter.Label(root, text='Region:', bg='#303136', fg="white", font=fontbig, anchor=tkinter.W).place(x=0, y=230, width=400, height=50)
+    tkinter.Label(root, text=f"{language['GPU:']} {savedsettings['gpuname']}", bg='#303136', fg="white", font=fontbig, anchor=tkinter.W).place(x=0, y=30, width=400, height=50)
+    tkinter.Label(root, text=language['Miner:'], bg='#303136', fg="white", font=fontbig, anchor=tkinter.W).place(x=0, y=80, width=400, height=50)
+    tkinter.Label(root, text=language['Algo:'], bg='#303136', fg="white", font=fontbig, anchor=tkinter.W).place(x=0, y=130, width=400, height=50)
+    tkinter.Label(root, text=language['Pool:'], bg='#303136', fg="white", font=fontbig, anchor=tkinter.W).place(x=0, y=180, width=400, height=50)
+    tkinter.Label(root, text=language['Region:'], bg='#303136', fg="white", font=fontbig, anchor=tkinter.W).place(x=0, y=230, width=400, height=50)
     #middle
     tkinter.Label(root, text='100°C', bg='#303136', fg="white", font=fontregular).place(x=325, y=550-100*5.2, width=50, height=30)
     tkinter.Label(root, text='90°C', bg='#303136', fg="white", font=fontregular).place(x=325, y=550-92.5*5.2, width=50, height=30)
@@ -63,19 +64,19 @@ def mainwindow():
     tkinter.Canvas(root, bg="#0A2133", highlightthickness=0).place(x=0, y=550 ,width=375, height=50)
     startbutton = tkinter.Button(root,fg="white",bg="#0A2133", border=0, font=fontregular, command=start, image=startbuttonanimation[1])
     startbutton.place(x=375,y=550,width=425,height=50)
-    tkinter.Button(root,fg="white", text= "Auto start",bg="red", border=2, font=fontregular).place(x=512,y=425,width=201,height=50)
+    tkinter.Button(root,fg="white", text=language['Auto start'],bg="red", border=2, font=fontregular).place(x=512,y=425,width=201,height=50)
     #top
     tkinter.Canvas(root, bg="#222129", highlightthickness=0).place(x=0,y=0,width=800,height=30)
-    tkinter.Button(root, text='Settings', bg='#222129', fg="white", border=1, font=fontregular, command=opensettings).place(x=0, y=0, width=188, height=30)
-    tkinter.Label(root, text='GPU Temperature', bg='#222129', fg="white", font=fontregular).place(x=200, y=0, width=400, height=30)
-    tkinter.Button(root, text='About us', bg='#222129', fg="white", border=1, font=fontregular, command=aboutus).place(x=612, y=0, width=188, height=30)
+    tkinter.Button(root, text=language['Settings'], bg='#222129', fg="white", border=1, font=fontregular, command=opensettings).place(x=0, y=0, width=188, height=30)
+    tkinter.Label(root, text=language['GPU Temperature'], bg='#222129', fg="white", font=fontregular).place(x=200, y=0, width=400, height=30)
+    tkinter.Button(root, text=language['About us'], bg='#222129', fg="white", border=1, font=fontregular, command=aboutus).place(x=612, y=0, width=188, height=30)
 
 
     root.title("Fruit Salad")
     root.iconbitmap(f'{pydir}\\3060.ico')
     root.geometry("800x600")
     root.resizable(False, False)
-    root.configure(bg='#303136')
+    
 
 
     root.lift()
@@ -83,68 +84,117 @@ def mainwindow():
     root.after_idle(root.attributes,'-topmost',False)
     root.protocol("WM_DELETE_WINDOW", windowclose)
     root.mainloop()
-    print('root dead')
 def trayicon():
     traymenu.run()
-    print('tray dead')
 def rgb_to_hex(rgb): return '%02x%02x%02x' % rgb
 def aboutus():
     global about
     global aboutopen
-    def openabout():
-        global about
-        def close():
-            global aboutopen
-            aboutopen = False
-            about.destroy()
-        def website(site):
-            if site == 1:
-                webbrowser.open("http://seflon.ddns.net/", new=2, autoraise=True)
-            if site == 2:
-                webbrowser.open("https://mezomgmt.com/", new=2, autoraise=True)
+    def close():
+        global aboutopen
+        aboutopen = False
+        about.destroy()
+    def website(site):
+        if site == 1:
+            webbrowser.open("http://seflon.ddns.net/", new=2, autoraise=True)
+        if site == 2:
+            webbrowser.open("https://mezomgmt.com/", new=2, autoraise=True)
+    if not aboutopen:
+        aboutopen = True
         about = tkinter.Toplevel()
+        about.title(language["About us"])
         about.geometry("600x400")
         about.resizable(False, False)
         about.iconbitmap(f'{pydir}\\3060.ico')
         about.configure(bg='#303136')
         #About looks
         tkinter.Label(about, text="Fruit Salad,", font=fontbig, bg='#303136', fg="white", anchor=tkinter.W).place(x=5, y=0)
-        tkinter.Label(about, text="a Salad mining manager.", font=fontregular, bg='#303136', fg="white", anchor=tkinter.W).place(x=10, y=30)
-        tkinter.Label(about, text="Made by Let Software", font=fontregular, bg='#303136', fg="white", anchor=tkinter.W).place(x=5, y=60)
-        tkinter.Label(about, text="with help by Mezo#0001 from MezoMGMT", font=fontregular, bg='#303136', fg="white", anchor=tkinter.W).place(x=5, y=100)
-        tkinter.Button(about, text="seflon.ddns.net", font=fontregular, bg='#4B4C54', fg="orange", anchor=tkinter.W, border=0, command= lambda: website(1)).place(x=150, y=60)
+        tkinter.Label(about, text=language["a Salad mining manager."], font=fontregular, bg='#303136', fg="white", anchor=tkinter.W).place(x=10, y=30)
+        tkinter.Label(about, text=language["Made by Let Software"], font=fontregular, bg='#303136', fg="white", anchor=tkinter.W).place(x=5, y=60)
+        tkinter.Label(about, text=language["with help by Mezo#0001 from MezoMGMT"], font=fontregular, bg='#303136', fg="white", anchor=tkinter.W).place(x=5, y=100)
+        tkinter.Button(about, text="seflon.ddns.net", font=fontregular, bg='#4B4C54', fg="orange", anchor=tkinter.W, border=0, command= lambda: website(1)).place(x=450, y=60)
         tkinter.Label(about, text="Let Software:", font=fontregular, bg='#303136', fg="white", anchor=tkinter.W).place(x=5, y= 140)
         tkinter.Label(about, text="Letronix624#9040 (Let)", font=fontregular, bg='#303136', fg="white", anchor=tkinter.W).place(x=15, y= 160)
         tkinter.Label(about, text="Nilsipilzi#9733 (brot)", font=fontregular, bg='#303136', fg="white", anchor=tkinter.W).place(x=15, y= 180)
-        tkinter.Button(about, text="mezomgmt.com", font=fontregular, bg='#4B4C54', fg="orange", anchor=tkinter.W, border=0, command= lambda: website(2)).place(x=275, y=100)
-
+        tkinter.Button(about, text="mezomgmt.com", font=fontregular, bg='#4B4C54', fg="orange", anchor=tkinter.W, border=0, command= lambda: website(2)).place(x=450, y=100)
         about.protocol("WM_DELETE_WINDOW", close)
-    if not aboutopen:
-        aboutopen = True
-        threading.Thread(target=openabout).start()
     else:
         about.deiconify()
         about.focus()
 def opensettings():#settings - settings - settings - settings - settings - settings - settings - settings - settings - settings - settings - settings - settings - settings - settings - settings - settings
     global settings
     global settingsopen
-    def opensettings():
-        global settings
-        def close():
-            global settingsopen
-            settingsopen = False
-            settings.destroy()
-        settings = tkinter.Toplevel()
+    def close():
+        global settingsopen
+        settingsopen = False
+        settings.destroy()
+    def openappsettings():
+        miningsettingsframe.place_forget()
+        advancedsettingsframe.place_forget()
+        appsettingsframe.place_forget()
+        megaguidesettingsframe.place_forget()
+        appsettingsframe.place(x=0, y=30, width=800, height=570)
+    def openminingsettings():
+        miningsettingsframe.place_forget()
+        advancedsettingsframe.place_forget()
+        appsettingsframe.place_forget()
+        megaguidesettingsframe.place_forget()
+        miningsettingsframe.place(x=0, y=30, width=800, height=570)
+    def openadvancedsettings():
+        miningsettingsframe.place_forget()
+        advancedsettingsframe.place_forget()
+        appsettingsframe.place_forget()
+        megaguidesettingsframe.place_forget()
+        advancedsettingsframe.place(x=0, y=30, width=800, height=570)
+    def openmegaguide():
+        miningsettingsframe.place_forget()
+        advancedsettingsframe.place_forget()
+        appsettingsframe.place_forget()
+        megaguidesettingsframe.place_forget()
+        megaguidesettingsframe.place(x=0, y=30, width=800, height=570)
+    def kickjesusfromchat():
+        changelang("Furry")
+        hhh.destroy()
+    def settingchange():
+        changelang(selectedlang.get())
+    if not settingsopen:
+        settingsopen = True
+        settings = tkinter.Toplevel(bg=defaultbg)
+        settings.title(language["Settings"])
         settings.geometry("800x600")
         settings.resizable(False, False)
         settings.iconbitmap(f'{pydir}\\3060.ico')
-        settings.configure(bg='#303136')
-        #nice cock
-        
         settings.protocol("WM_DELETE_WINDOW", close)
-    if not settingsopen:
-        settingsopen = True
-        threading.Thread(target=opensettings).start()
+        #nice cock ----- Layout ----- settings layout. Mining settings, app settings, advanced settings, SECRET SETTINGS
+        appsettingsframe = tkinter.Frame(settings, bg=defaultbg)
+        miningsettingsframe = tkinter.Frame(settings, bg=defaultbg)
+        advancedsettingsframe = tkinter.Frame(settings, bg=defaultbg)
+        megaguidesettingsframe = tkinter.Frame(settings, bg="pink")
+        appsettingsframe.place(x=0, y=30, width=800, height=570)
+        tkinter.Button(settings, text=language["App Settings"], font=fontregular, bg='#222129', fg="white", command=openappsettings).place(x=0, y=0, width=300, height=30)
+        tkinter.Button(settings, text=language["Mining Settings"], font=fontregular, bg='#222129', fg="white", command=openminingsettings).place(x=300, y=0, width=300, height=30)
+        tkinter.Button(settings, text=language["Advanced Settings"], font=fontregular, bg='#222129', fg="white", command=openadvancedsettings).place(x=600, y=0, width=200, height=30)
+        tkinter.Button(settings, border=0, bg=defaultbg, command=openmegaguide).place(x=790, y=590, width=10, height=10)
+        tkinter.Button(settings, text=language['Accept Settings'], command=settingchange).place(x=595, y=565, width=200, height=30)
+        #App Settings
+        selectedlang = tkinter.StringVar()
+        selectedlang.set(savedsettings['language'])
+        hhhh = tkinter.OptionMenu(appsettingsframe, selectedlang, *supportedlanguages)
+        hhhh.place(x=5, y=3, width=100, height=24)
+        hhhh.configure(highlightthickness=0)
+        tkinter.Label(appsettingsframe, bg=defaultbg, fg="white", font=fontregular, text=language["Language. Apply by closing the program in task manager and reopening."], anchor=tkinter.W).place(x=150, y=5, width=800, height=20)
+        #Mining Settings
+        #Advanced Settings
+        #Megaguide Settings
+        tkinter.Label(megaguidesettingsframe, text=language["Secret Settings"], bg="pink", fg="white", font=fontextremelybig).pack()
+        tkinter.Button(megaguidesettingsframe, text="Megaguide", bg="Red", fg="White", command=megaguide, font=fontregular, padx=10, pady=5).pack(anchor=tkinter.NW)
+        if savedsettings['language'] != "Furry":
+            tkinter.Label(megaguidesettingsframe, text=language["Button below requires Task Manager Restart."], font=fontregular).pack(anchor=tkinter.NW)
+            hhh = tkinter.Button(megaguidesettingsframe, text=language["Furry Language"], bg="yellow", fg="pink", font=fontregular, command=kickjesusfromchat)
+            hhh.pack(anchor=tkinter.NW) #messageinblood
+        
+
+        
     else:
         settings.deiconify()
         settings.focus()
@@ -211,11 +261,14 @@ def windowopen():
         root.deiconify()
 #def byebye():
     #root.destroy()
-def changegpu():
-    global gpuname
-    gpuname['gpuname'] = nvidia.get()
+def changesettings():
+    savedsettings['gpuname'] = nvidia.get()
+    savedsettings['language'] = defaultlang.get()
+    changelang(savedsettings['language'])
+    
+
     with open(f"{pydir}\\settings.json", ("w")) as settings:
-                settings.write(json.dumps(gpuname))
+                settings.write(json.dumps(savedsettings))
     gpudefine.destroy()
 
 def gputemp():
@@ -224,20 +277,20 @@ def gputemp():
             temp = c.Hardware[0].Sensors[a].get_Value()
             c.Hardware[0].Update()
             return temp
+def changelang(lang):
+    global language
+    savedsettings["language"] = lang
+    with open(f"{pydir}\\languages\\{lang}.json") as data:
+        language = json.load(data)
+    with open(f"{pydir}\\settings.json", ("w")) as settings:
+        settings.write(json.dumps(savedsettings))
+    threading.Thread(target=mainwindow)
 icon = Image.open(f"{pydir}\\3060.ico")
 mining = False
 fontregular = (f"{pydir}\\GUI\\BarlowCondensed-Medium.ttf", 10, "bold")
 fontbig = (f"{pydir}\\GUI\\BarlowCondensed-Medium.ttf", 20, "bold")
 fontextremelybig = (f"{pydir}\\GUI\\BarlowCondensed-Medium.ttf", 50, "bold")
 a = ""
-hashrate = 0
-aboutopen = False
-settingsopen = False
-gpuname = {'gpuname':''}
-gpus = GPUtil.getGPUs()
-c = Computer()
-c.GPUEnabled = True
-c.Open()
 supportedgpus = [
     "GTX 1050",
     "GTX 1050 Ti",
@@ -267,38 +320,57 @@ supportedgpus = [
     "RTX 3080 TI",
     "RTX 3090",    
 ]
+supportedlanguages = [
+    "English",
+    "Deutsch",
+]
+defaultbg = "#303136"
+hashrate = 0
+aboutopen = False
+settingsopen = False
+savedsettings = {'gpuname':'', 'language':''}
+gpus = GPUtil.getGPUs()
+c = Computer()
+c.GPUEnabled = True
+c.Open()
+try:
+    with open(f"{pydir}\\settings.json", "r") as data:
+        savedsettings = json.load(data)
+        changelang(savedsettings["language"])
+except Exception as e:
+    print(e)
+    savedsettings["language"] = "English"
+    savedsettings['gpuname'] = "Unknown"
+    gpudefine = tkinter.Tk()
+    nvidia = tkinter.StringVar()
+    nvidia.set("Nvidia")
+    defaultlang = tkinter.StringVar()
+    defaultlang.set("English")
+    tkinter.Label(gpudefine, text='Startup settings.', bg='#303136', fg="white", font=fontregular).pack()
+    tkinter.Label(gpudefine, text="What gpu do you use.").pack()
+    tkinter.OptionMenu(gpudefine, nvidia, *supportedgpus).pack()
+    tkinter.Label(gpudefine, text="Language.").pack()
+    tkinter.OptionMenu(gpudefine, defaultlang, *supportedlanguages).pack()
+    tkinter.Button(gpudefine, text="Done", command=changesettings).pack()
+
+    gpudefine.title("Fruit Salad")
+    gpudefine.iconbitmap(f'{pydir}\\3060.ico')
+    gpudefine.geometry("600x400")
+    gpudefine.resizable(False, False)
+    gpudefine.configure(bg='#303136')
+    gpudefine.mainloop()
 traymenucontent = (
     item('Show or hide', windowopen, default=True, visible=False),
-    item('Hide', windowclose, default=False),
+    item(language['Hide'], windowclose, default=False),
     #item('Quit', byebye, default=False)
 )
+if savedsettings["gpuname"] == "Unknown":
+    savedsettings["gpuname"] == language["Unknown"]
 traymenu = pystray.Icon("Fruit Salad", icon, "Fruit Salad", traymenucontent)
 
 
 
 if __name__ == "__main__":
-    for gpu in gpus:
-        gpuname['gpuname'] = gpu.name
-    if gpuname['gpuname'] == "":
-        try:
-            with open(f"{pydir}\\settings.json", ("r")) as settings:
-                gpuname['gpuname'] = json.load(settings)["gpuname"]
-        except:
-            gpuname['gpuname'] = "Unknown"
-            gpudefine = tkinter.Tk()
-            nvidia = tkinter.StringVar()
-            nvidia.set("Nvidia")
-            tkinter.Label(gpudefine, text=f'No GPU Detected. Select your GPU.', bg='#303136', fg="white", font=fontregular).pack()
-            tkinter.OptionMenu(gpudefine, nvidia, *supportedgpus).pack()
-            tkinter.Button(gpudefine, text="Select", command=changegpu).pack()
-
-            gpudefine.title("Fruit Salad")
-            gpudefine.iconbitmap(f'{pydir}\\3060.ico')
-            gpudefine.geometry("600x400")
-            gpudefine.resizable(False, False)
-            gpudefine.configure(bg='#303136')
-            gpudefine.mainloop()
-
     traythread = threading.Thread(target=trayicon)
     traythread.start()
     threading.Thread(target=mainwindow).start()
